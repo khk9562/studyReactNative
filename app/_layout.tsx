5ec7baa17abc0a6084d7d3e1359b58a6859ddcf7
stella <khk9562@naver.com>
2024-05-30 14:43:22 +0900

    expo router 와 react navigation 차이 확인

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,11 +5,16 @@ import {
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+// import HomeScreen from "./(tabs)";
+
+// const Stack = createNativeStackNavigator();
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -40,6 +45,12 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+
+      {/* <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen component={HomeScreen} name="Home" />
+        </Stack.Navigator>
+      </NavigationContainer> */}
     </ThemeProvider>
   );
 }
